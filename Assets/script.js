6693faa81c894338d77700da3708d2a023809230
Victoria Dunlap <victoriacdunlap@gmail.com>
2022-07-04 10:42:07 -0400

    Set up functions for color block changes. Setting up local storage

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,90 @@
 var todayDateAndTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+// var nineAm = $('#9am');
+var inputValues = $('.input')
+var saveButton = $('.button')
+
 
 // adds date and time to the page 
-
 var timeUpdate = () => {
     $("#currentDay").text(todayDateAndTime);
 }
 setInterval(timeUpdate, 1000);
 
+
+// makes block red when it is within the current hour 
+function colorChange () {
+    var now = Date().getHours();
+    if (now >= 9 || now < 10) {
+        document.body.style.backgroundColor = "red"
+    }
+    if (now >= 10 || now < 11) {
+        document.body.style.backgroundColor = "red"
+    }
+    if (now >= 11 || now < 12) {
+        document.body.style.backgroundColor = "red"
+    }
+    if (now >= 12 || now < 13) {
+        document.body.style.backgroundColor = "red"
+    }
+    if (now >= 13 || now < 14) {
+        document.body.style.backgroundColor = "red"
+    }
+    if (now >= 14 || now < 15) {
+        document.body.style.backgroundColor = "red"
+    }
+    if (now >= 15 || now < 16) {
+        document.body.style.backgroundColor = "red"
+    }
+    if (now >= 16 || now < 17) {
+        document.body.style.backgroundColor = "red"
+    }
+    // if (now >= 1 || now < 11) {
+    //     document.body.style.backgroundColor = "red"
+    // }
+    return (colorChange)
+}
+
+// changes color to grey when it is past that time 
+function colorChangeForPastTime () {
+    var now = Date().getHours();
+    if (now >= 9) {
+       document.getElementById(nineAm).style.backgroundColor = "grey"
+    }
+    if (now >= 10) {
+        document.getElementById(nineAm).style.backgroundColor = "grey"
+     }
+     if (now >= 11) {
+        document.getElementById(nineAm).style.backgroundColor = "grey"
+     }
+     if (now >= 12) {
+        document.getElementById(nineAm).style.backgroundColor = "grey"
+     }
+     if (now >= 13) {
+        document.getElementById(nineAm).style.backgroundColor = "grey"
+     }
+     if (now >= 14) {
+        document.getElementById(nineAm).style.backgroundColor = "grey"
+     }
+     if (now >= 15) {
+        document.getElementById(nineAm).style.backgroundColor = "grey"
+     }
+     if (now >= 16) {
+        document.getElementById(nineAm).style.backgroundColor = "grey"
+     }
+     if (now >= 17) {
+        document.getElementById(nineAm).style.backgroundColor = "grey"
+     }
+    return (colorChangeForPastTime)
+}
+
+// has the document save the input when the button is clicked once 
+var eventOnSchedule = () => {
+    saveButton.onclick.addEventListener('click', addEvent);
+    localStorage.getItem(inputValues)
+    console.log('am i crazy?')
+}
+
+// add the event to the page 
+function addEvent () {
+    document.appendChild(eventOnSchedule)
+}
